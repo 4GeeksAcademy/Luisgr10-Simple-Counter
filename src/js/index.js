8de50d40a6a35f5f6b68c161ae4e0d1838d93bfe
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-01-29 17:39:03 +0000

    Actualizacion 1.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Boxes from "./component/Boxes.jsx";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
